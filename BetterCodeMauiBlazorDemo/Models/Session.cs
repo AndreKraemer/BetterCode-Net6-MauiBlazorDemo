5cdff618a080363ccd1978270bd5fe623dccd40d
André Krämer <andre.kraemer@qualitybytes.de>
2021-11-23 16:11:29 +0100

    Projektdateien hinzufügen.

---
+++ BetterCodeMauiBlazorDemo/Models/Session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BetterCodeMauiBlazorDemo.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<string> Goals { get; set; }
+        public Speaker Speaker { get; set; }
+
+        public TimeOnly Start { get; set; }
+        public TimeOnly End { get; set; }
+
+    }
+}
---
+++ BetterCodeMauiBlazorDemo/Services/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using BetterCodeMauiBlazorDemo.Models;
+using System.Collections.Generic;
+
+namespace BetterCodeMauiBlazorDemo.Services
+{
+    public interface IDataService
+    {
+        Session GetSession(int id);
+        IEnumerable<Session> GetSessions();
+        Speaker GetSpeaker(int id);
+        IEnumerable<Speaker> GetSpeakers();
+    }
+}
